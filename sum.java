e13ffb3c5e3d45a3ac6f4f9b553b925b1b8f2111
Ashwani <ashwani271101@gmail.com>
2023-04-06 11:11:27 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class sum{
+
+    public static void main(String[] args) {
+         System.out.println("Enter the number of a ");
+        Scanner sc =  new Scanner(System.in);
+         
+       
+        int a = sc.nextInt();
+        System.out.println("Enter the number of b ");
+        int b = sc.nextInt();
+        System.out.println("Addition : " + (a+b));
+        
+    }
+}
