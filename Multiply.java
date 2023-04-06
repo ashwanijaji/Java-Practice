81dcd71878362c6048eb6c7abbcbe2a552167fde
Ashwani <ashwani271101@gmail.com>
2023-04-06 11:32:23 +0530

    multiply feature added

---
+++ Multiply.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Multiply {
+    public static void main(String[] args) {
+        System.out.println("Enter the number of a ");
+       Scanner sc =  new Scanner(System.in);
+        
+      
+       int a = sc.nextInt();
+       System.out.println("Enter the number of b ");
+       int b = sc.nextInt();
+       System.out.println("Multiplication: " + (a*b));
+       
+   }
+}
